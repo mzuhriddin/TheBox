87c234299121721c4fb9a05404dfd76045c08e72
@zuhriddin.musabayev.2002 <zuhriddin.musabayev.2002@gmail.com>
2022-09-21 15:30:08 +0500

    finished responsive screen for (768px - 1023px) sizes

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const button = document.createElement('button');
-const dropdown = document.querySelector("#dropdown");
-dropdown.appendChild(button);
+let x = document.getElementById("drop-down");
+let htmlDivElement = document.createElement("div");
+let bodyContainer = document.getElementById("body-container");
 
+function menuShow(event) {
+    event.stopPropagation();
+    htmlDivElement.classList.add("hamburger");
+    htmlDivElement.style.visibility = "visible";
+    htmlDivElement.style.animation = "show 1s";
+    bodyContainer.style.opacity = "0.6";
+    htmlDivElement.append(x);
+    document.body.append(htmlDivElement);
+}
 
-console.log(dropdown)
+function menuClose() {
+    if (htmlDivElement.classList.contains("hamburger")) {
+        htmlDivElement.style.animation = "hide 1s";
+        bodyContainer.style.opacity = "1";
+        setTimeout(() => {
+            htmlDivElement.classList.remove("hamburger");
+        }, 1000);
+    }
+
+}
+
+document.querySelector(".icon").addEventListener("click", menuShow);
+document.querySelector("#body-container").addEventListener("click", menuClose)
