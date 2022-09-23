bd683ba1384247eaba255931c28b05ed12255491
@zuhriddin.musabayev.2002 <zuhriddin.musabayev.2002@gmail.com>
2022-09-23 11:59:58 +0500

    finished responsive screen for (425px - 767px) sizes

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let x = document.getElementById("drop-down");
-let htmlDivElement = document.createElement("div");
+let htmlDivElement = document.getElementById("div");
 let bodyContainer = document.getElementById("body-container");
+let xButton = document.createElement("i");
+let htmlAnchorElement = document.getElementById("a");
+xButton.classList.add("fa-solid", "fa-xmark");
+htmlAnchorElement.classList.add("x-mark");
+htmlAnchorElement.append(xButton);
+
 
 function menuShow(event) {
     event.stopPropagation();
-    htmlDivElement.classList.add("hamburger");
-    htmlDivElement.style.visibility = "visible";
+    htmlDivElement.style.display = "block";
     htmlDivElement.style.animation = "show 1s";
     bodyContainer.style.opacity = "0.6";
+    htmlDivElement.append(htmlAnchorElement);
     htmlDivElement.append(x);
     document.body.append(htmlDivElement);
 }
@@ -17,11 +23,14 @@ function menuClose() {
         htmlDivElement.style.animation = "hide 1s";
         bodyContainer.style.opacity = "1";
         setTimeout(() => {
-            htmlDivElement.classList.remove("hamburger");
+            htmlAnchorElement.remove();
+            htmlDivElement.remove();
         }, 1000);
     }
 
 }
 
 document.querySelector(".icon").addEventListener("click", menuShow);
-document.querySelector("#body-container").addEventListener("click", menuClose)
+document.querySelector("#body-container").addEventListener("click", menuClose);
+document.querySelector(".x-mark").addEventListener("click", menuClose);
+
